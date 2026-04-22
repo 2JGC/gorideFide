2b537d2dff06ecc1c8ff1aea09f52b3de77b48c7
alli00152 <allison.alvarado2007@gmail.com>
2026-04-21 23:28:54 -0600

    Servicios Motocicletas y Reservas

--- src/main/java/com/goridemoto/service/ReservaService.java
+++ src/main/java/com/goridemoto/service/ReservaService.java
@@ -1,7 +1,11 @@
 package com.goridemoto.service;
 
 import com.goridemoto.domain.Reserva;
+import java.util.List;
 
 public interface ReservaService {
     void guardar(Reserva reserva);
+    List<Reserva> listar();
+    void actualizarEstado(Long reservaId, String estado);
+    List<Reserva> listarPorUsuario(String emailUsuario);
 }
