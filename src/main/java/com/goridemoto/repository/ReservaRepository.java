712fe3a49e989c170015879d6dc338c9a368d001
alli00152 <allison.alvarado2007@gmail.com>
2026-04-21 23:22:42 -0600

    Motocicletas y Reservas

--- src/main/java/com/goridemoto/repository/ReservaRepository.java
+++ src/main/java/com/goridemoto/repository/ReservaRepository.java
@@ -1,7 +1,9 @@
 package com.goridemoto.repository;
 
 import com.goridemoto.domain.Reserva;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    List<Reserva> findByEmailUsuarioOrderByFechaCreacionDesc(String emailUsuario);
 }
